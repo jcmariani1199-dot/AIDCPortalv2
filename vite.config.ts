eed3706d5e31016f3cdf2fa71bb00c88497bcadd
jcmariani1199-dot <jcmariani1199@gmail.com>
2025-10-02 10:12:24 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  // VVV THIS IS THE LINE I ADDED VVV
+  base: '/AIDCPortalv2/',
+
   plugins: [
     react(),
     runtimeErrorOverlay(),
