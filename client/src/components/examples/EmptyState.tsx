f3a1eb53ddf81b48c1e9fc0df1e578defa1b7e3e
jcmariani1199-dot <jcmariani1199@gmail.com>
2025-10-02 12:41:04 +0100

    Delete client directory

--- client/src/components/examples/EmptyState.tsx
+++
@@ -1,9 +0,0 @@
-import EmptyState from "../EmptyState";
-
-export default function EmptyStateExample() {
-  return (
-    <div className="min-h-screen bg-background">
-      <EmptyState onUpload={() => console.log("Upload triggered")} />
-    </div>
-  );
-}
